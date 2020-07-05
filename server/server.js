51d452198f18a096459aac647cf83abeb3657a57
Wani Joseph <waynekhlifa.wk@gmail.com>
2020-07-05 14:12:02 +0200

    serves frontend requests

--- server/server.js
+++ server/server.js
@@ -29,6 +29,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api", authRoutes);
 
+// DEFAULT
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () =>
